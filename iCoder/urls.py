72abbaefa93e11c8331c9a72a336692d53b39b75
ayushrathore9 <71789339+ayushrathore9@users.noreply.github.com>
2020-10-01 01:07:20 +0530

    Update urls.py

--- iCoder/urls.py
+++ iCoder/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# This is the parent file of all the files
+
 from django.contrib import admin
 from django.urls import path, include
 
